a3f21acbf5633e26c0e3ca3826a06157bdd3d6b7
pelDev <pelumidevelops18@gmail.com>
2022-04-07 07:48:33 -0700

    Error Handler

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,33 +1,10 @@
-const ErrorResponse = require("../utils/ErrorResponse");
-
 const errorHandler = (err, req, res, next) => {
   console.log(err);
 
   let error = { ...err };
   error.message = err.message;
 
-  //   // Bad ObjectId
-  //   if (err.name === "CastError") {
-  //     const message = `${err.resource} not found with id of ${err.value}`;
-
-  //     error = new ErrorResponse(message, 404);
-  //   }
-
-  //   // Mongoose Duplicate key
-  //   if (err.code === 11000) {
-  //     const message = "Duplicate field entered";
-  //     error = new ErrorResponse(message, 400);
-  //   }
-
-  //   // Mongoose Validation Error
-  //   if (err.name === "ValidationError") {
-  //     const message = Object.values(err.errors).map((val) => val.message);
-  //     error = new ErrorResponse(message, 400);
-  //   }
-
-  res
-    .status(error.statusCode || 500)
-    .json({ success: false, error: error.message || "Server error" });
+  res.status(error.statusCode || 500).send(`Error ${error.message}`);
 };
 
 module.exports = errorHandler;
